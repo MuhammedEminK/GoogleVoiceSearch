3f179ac9562242fafab24a0e804ab4a8b58b656e
MuhammedEminK <83522521+MuhammedEminK@users.noreply.github.com>
2022-01-09 16:24:50 +0300

    Delete seleniımsdasd.py

--- GoogleVoiceSearch/seleniımsdasd.py
+++
@@ -1,8 +0,0 @@
-
-from selenium import webdriver
-import time
-import threading
-driver = webdriver.Chrome()
-driver.get("https://stackoverflow.com/questions/25394329/python-voice-recognition-library-always-listen")
-
-driver.close()
